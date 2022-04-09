c1ac1dec43f530a361cf2ac38a2522db1249b4d2
unknown <l.cliniomaraia@gmail.com>
2022-04-09 10:59:20 -0300

    adicionando conexão ao banco de dados

--- conexao.php
+++ conexao.php
@@ -2,4 +2,4 @@
 
 //host, usuário, senha, nome do banco
 
-$conn = mysqli_connect('localhost', '', '', '') or die('Erro ao conectar ao banco de dados');
+$conn = mysqli_connect('us-cdbr-east-05.cleardb.net', 'bfeff8e430695b', '635a721', 'heroku_e908e3585c43f11') or die('Erro ao conectar ao banco de dados');
